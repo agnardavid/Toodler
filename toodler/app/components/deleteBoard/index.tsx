18a3b6779436aca212df3d0e6ca9540935728697
Agnar Davíð Halldórsson <agnardavid@hotmail.com>
2024-11-28 15:07:31 +0000

    imports changed and delete made async

--- toodler/app/components/deleteBoard/index.tsx
+++ toodler/app/components/deleteBoard/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { TouchableOpacity, Text } from "react-native";
 import styles from "./styles";
-//import { deleteBoard } from "@/app/Services/JsonInterpreter";
+import { deleteBoard } from "@/app/Services/JsonInterpreter";
 
 interface DeleteButtonProps {
     boardId: number;
